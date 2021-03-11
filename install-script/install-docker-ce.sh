8b4eb944e25ff19b507948434fb76678675f7935
Riki Permana <arwahdevops@gmail.com>
2021-03-12 00:42:40 +0700

    rename folder

---
+++ install-script/install-docker-ce.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+#Developer : Riki Permana
+#install docker CE
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce
+#install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
